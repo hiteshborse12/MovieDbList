cd8f166e92e5206cd9df474cbb34aa3812182361
Hitesh <Hitesh.borse@wiccon.se>
2020-11-05 14:24:51 +0530

    Implemented search algorithm

--- MovieDbList/Utilities/Extensions/String.swift
+++ MovieDbList/Utilities/Extensions/String.swift
@@ -10,7 +10,39 @@ extension String{
     
     // Search for movies in the movie list
     func searchMovies(movies:[MovieModel]) -> [MovieModel]{
-        return [MovieModel]()
+        // Split search string into non empty string array
+        let searchArray  = self.components(separatedBy: .whitespacesAndNewlines).filter {
+            if ($0.count > 0) {
+                return true
+            }
+            return false
+        }
+        // Get final matched movie arrray
+        let matchedMovieArray =  movies.filter { (movie) -> Bool in
+            
+            // Get array of matched search text found in movie title
+            let matchedSearchs = searchArray.filter { (searchText) -> Bool in
+                
+                // Return true only if movie title any sub string start search text
+                return movie.title?.isStringStartWith(subString: searchText) ?? false
+            }
+            
+            // Return true only if movie title contains all search text
+            return (matchedSearchs.count > 0  && matchedSearchs.count >= searchArray.count)
+        }
+        return matchedMovieArray
+    }
+    
+    // Check if string start with subString
+    func isStringStartWith(subString:String) -> Bool {
+        let result = self.components(separatedBy: .whitespacesAndNewlines).filter { (string) -> Bool in
+            // Check string prefix
+            if string.lowercased().hasPrefix(subString.lowercased()){
+                return true
+            }
+            return false
+        }
+        return (result.count > 0)
     }
     
 }
